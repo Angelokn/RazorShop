a2dc0027d0d2b06728253ba3c445ce13c2503fe5
Angelo <angelo.knto@gmail.com>
2023-11-24 15:33:31 -0300

    populated Item model

--- RazorShop/Models/Item.cs
+++ RazorShop/Models/Item.cs
@@ -1,7 +1,30 @@
-﻿namespace RazorShop.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorShop.Models
 {
     public class Item
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(35)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+
 
     }
 }
