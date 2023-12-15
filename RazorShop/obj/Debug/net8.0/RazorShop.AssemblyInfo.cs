48428ce4561745a9b893a7e5cc20fab1b50ebc05
Angelo <angelo.knto@gmail.com>
2023-12-15 16:55:41 -0300

    added route link to access Category in the Home page

--- RazorShop/obj/Debug/net8.0/RazorShop.AssemblyInfo.cs
+++ RazorShop/obj/Debug/net8.0/RazorShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d4eef974bbd0a5603d3f4896089e426d3cc8e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
