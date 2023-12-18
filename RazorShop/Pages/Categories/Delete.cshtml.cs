4af9d1646a8f7f3802d0b12519520589b5a97942
Angelo <angelo.knto@gmail.com>
2023-12-18 18:59:10 -0300

    created Update and Delete Razor pages and its respective cs files

---
+++ RazorShop/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorShop.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
