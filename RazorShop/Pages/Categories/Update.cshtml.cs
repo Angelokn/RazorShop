6b5910d324a951486113512e7d84228031e8ae4b
Angelo <angelo.knto@gmail.com>
2023-12-18 19:17:12 -0300

    implemented Update razor page and cs methods

--- RazorShop/Pages/Categories/Update.cshtml.cs
+++ RazorShop/Pages/Categories/Update.cshtml.cs
@@ -1,12 +1,38 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorShop.Data;
+using RazorShop.Models;
 
 namespace RazorShop.Pages.Categories
 {
     public class UpdateModel : PageModel
     {
+        [BindProperty]
+        public Category Category { get; set; }
+
+        private readonly ApplicationDbContext _db;
+
+        public UpdateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
+        }
+
+        public IActionResult OnUpdate(int id)
+        {
+            var category = _db.Categories.FirstOrDefault(u => u.Id == id);
+
+            if (category == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            _db.Update(category);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
--- RazorShop/obj/Debug/net8.0/RazorShop.AssemblyInfo.cs
+++ RazorShop/obj/Debug/net8.0/RazorShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af9d1646a8f7f3802d0b12519520589b5a97942")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
