1c667d4869cb4d7123ca028ddccc201e7bf43015
Angelo <angelo.knto@gmail.com>
2023-12-19 20:01:37 -0300

    implemented Delete category method

--- RazorShop/Pages/Categories/Delete.cshtml.cs
+++ RazorShop/Pages/Categories/Delete.cshtml.cs
@@ -1,12 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorShop.Data;
+using RazorShop.Models;
 
 namespace RazorShop.Pages.Categories
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        public Category Category { get; set; }
+
+        private readonly ApplicationDbContext _db;
+        public DeleteModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        public void OnGet(int id)
+        {
+            if (id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(Category);
+                _db.SaveChanges();
+
+                return RedirectToPage("Index");
+            }
+
+            return Page();
         }
     }
 }
