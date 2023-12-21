c9827447b37d2a40b5c6220090550eb3c7887da4
Angelo <angelo.knto@gmail.com>
2023-12-21 17:30:23 -0300

    implemented initial Index razor page and cs file

--- RazorShop/Pages/Items/Index.cshtml.cs
+++ RazorShop/Pages/Items/Index.cshtml.cs
@@ -1,10 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorShop.Data;
+using RazorShop.Models;
 
 namespace RazorShop.Pages.Items
 {
     public class IndexModel : PageModel
     {
+        public List<Item> ItemList { get; set; }
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
         }
