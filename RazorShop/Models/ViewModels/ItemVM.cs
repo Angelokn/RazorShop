7b0bd11263c4242f4e2879c7cc064b11f5c9b462
Angelo <angelo.knto@gmail.com>
2024-01-15 19:44:14 -0300

    created ItemVM model

---
+++ RazorShop/Models/ViewModels/ItemVM.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace RazorShop.Models.ViewModels
+{
+    public class ItemVM
+    {
+        public Item Item { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+    }
+}
