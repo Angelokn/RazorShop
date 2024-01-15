530a53ed1cf0116e29960a223e192930799421f9
Angelo <angelo.knto@gmail.com>
2024-01-15 18:52:06 -0300

    implemented Item - Create.cs file

--- RazorShop/Pages/Items/Create.cshtml.cs
+++ RazorShop/Pages/Items/Create.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorShop.Data;
+using RazorShop.Models;
 
 namespace RazorShop.Pages.Items
 {
     public class CreateModel : PageModel
     {
+        public Item Item { get; set; }
+
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Items.Add(Item);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
