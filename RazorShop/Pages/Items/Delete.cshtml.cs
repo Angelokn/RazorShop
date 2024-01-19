08a79837b2ae31236cc585922d74db98c249f098
Angelo <angelo.knto@gmail.com>
2024-01-19 14:54:01 -0300

    implemented Item - Delete cs file

--- RazorShop/Pages/Items/Delete.cshtml.cs
+++ RazorShop/Pages/Items/Delete.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorShop.Data;
+using RazorShop.Models;
 
 namespace RazorShop.Pages.Items
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        public Item Item { get; set; }
+
+        private readonly ApplicationDbContext _db;
+
+        public DeleteModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+        public void OnGet(int id)
+        {
+            Item = _db.Items.Find(id);
+        }
+
+        public IActionResult OnPost(Item item)
+        {
+            _db.Items.Remove(item);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
