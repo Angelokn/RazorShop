ed6e3d25556a786f2e2990a3f76b4276fc494729
Angelo <angelo.knto@gmail.com>
2024-01-20 17:04:55 -0300

    update for a few minor changes

--- RazorShop/obj/Debug/net8.0/RazorShop.AssemblyInfo.cs
+++ RazorShop/obj/Debug/net8.0/RazorShop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorShop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f56c25523fe9fc844e8c5dc5d9d0d105ea3315d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bb8714062fcf37505176651e7af61dadf42adda")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorShop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorShop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
